02e216b833802092e42d1d63692e4619cbb27f0b
Tirumala Teja <tirumalateja.jampani@gmail.com>
2026-06-07 14:56:23 +0530

    Refactor NavBar component layout and structure

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-// import { SERVER_URL } from "../utils/constants";
 import { removeUser } from "../utils/userSlice";
+
 const NavBar = () => {
   const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
@@ -18,23 +18,43 @@ const NavBar = () => {
       console.error(err);
     }
   };
+
   return (
     <div>
       <div className="navbar bg-base-300 shadow-sm py-3">
         <div className="flex-1">
-          <Link to={"/"} className="btn btn-ghost text-xl capitalize">
+          <Link to="/" className="btn btn-ghost text-xl capitalize">
             👨🏻‍💻 DevTinder
           </Link>
         </div>
         {user && (
-          <div className="flex-1 justify-end">
+          <div className="flex-1 justify-center">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/connections">Connections</Link>
+              </li>
+              <li>
+                <Link to="/requests">Requests</Link>
+              </li>
+              <li>
+                <button onClick={handleLogout}>Logout</button>
+              </li>
+            </ul>
+          </div>
+        )}
+
+        {user && (
+          <div className="flex items-center gap-4">
             <h5 className="capitalize font-semibold">
               Welcome {user?.firstName}
             </h5>
-          </div>
-        )}
-        <div className="flex">
-          {user && (
+
             <div className="dropdown dropdown-end mx-4">
               <div
                 tabIndex={0}
@@ -45,29 +65,33 @@ const NavBar = () => {
                   <img alt="UserImage" src={user?.photoUrl} />
                 </div>
               </div>
+
               <ul
-                tabIndex="-1"
+                tabIndex={-1}
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
               >
                 <li>
-                  <Link to={"/profile"} className="justify-between">
+                  <Link to="/profile" className="justify-between">
                     Profile
                     <span className="badge">New</span>
                   </Link>
                 </li>
+
                 <li>
-                  <Link to={"/connections"}>Connections</Link>
+                  <Link to="/connections">Connections</Link>
                 </li>
+
                 <li>
-                  <Link to={"/requests"}>Requests</Link>
+                  <Link to="/requests">Requests</Link>
                 </li>
+
                 <li>
                   <Link onClick={handleLogout}>Logout</Link>
                 </li>
               </ul>
             </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </div>
   );
